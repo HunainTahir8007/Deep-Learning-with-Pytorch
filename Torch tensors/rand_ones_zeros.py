80f3ba2294aa0d9c02bd2ed79b26434e7d1bee51
Muhammad Hunain <mhunain8007@gmail.com>
2026-02-15 10:27:43 -0800

    Add files via upload

---
+++ Torch tensors/rand_ones_zeros.py
@@ -0,0 +1,12 @@
+import torch 
+import numpy 
+
+shape=(2,4)
+
+random=torch.rand(shape)
+ones=torch.ones(shape)
+zeros=torch.zeros(shape)
+
+print("random",random)
+print("ones",ones)
+print('zeros',zeros)
